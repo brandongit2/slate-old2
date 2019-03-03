b28b589f1113b8624089ed53d07cdfb71a5bbe83
Michael Pu <michael.pu123@gmail.com>
2019-03-03 13:31:08 -0500

    Fix typo

--- backend/src/server.test.js
+++ backend/src/server.test.js
@@ -3,7 +3,7 @@ const app = require('./server');
 
 describe('Slate API', function() {
     describe('/api', () => {
-        interface('should return 200', (done) => {
+        it('should return 200', (done) => {
             request(app).get('/api').expect(200, done);
         })
     });
