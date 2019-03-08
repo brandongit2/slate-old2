489429f6854e3d23269e2eddc45e174ac812e681
Michael Pu <michael.pu123@gmail.com>
2019-03-08 17:48:59 -0500

    Fix setup test env script

--- backend/test/setupTestEnv.sh
+++ backend/test/setupTestEnv.sh
@@ -1,5 +1,5 @@
 cd ../
 cp ./test/config.test.json ./src/config.json
-mysql -e 'CREATE DATABASE slate;'
-cat migrations/* | mysql slate
-cat ./test/slate.test.sql | mysql slate
+mysql -u root -e 'CREATE DATABASE slate;'
+cat migrations/* | mysql slate -u root
+cat ./test/slate.test.sql | mysql slate -u root
