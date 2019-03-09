40c23e3be276d6e5e8b60acb1f4dcbe828e8449a
Henry Gu <1520879+0@users.noreply.github.com>
2019-03-08 20:15:48 -0500

    Allow h1/h2 tags in page content

--- backend/src/data.js
+++ backend/src/data.js
@@ -21,7 +21,7 @@ function parseContent(content) {
 
     content = sanitizeHtml(content, {
         allowedTags: [
-            'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
+            'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
             'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
             'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'iframe',
             'svg', 'path', 'span'
