b7625f8131c5cd88efdecfd5c68d00f095609f0e
Michael Pu <michael.pu123@gmail.com>
2019-03-09 14:21:38 -0500

    Run travis ci in parallel

--- test/setup-tests.sh
+++ test/setup-tests.sh
@@ -4,3 +4,5 @@ sudo apt-get install nginx
 sudo mv slate.nginx.test.conf /etc/nginx/sites-available/default
 sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 sudo nginx
+
+
