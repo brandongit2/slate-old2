794a5a0c4c6aa5420254d920a5f302e9be005bd1
Michael Pu <michael.pu123@gmail.com>
2019-03-09 15:19:13 -0500

    Fixed logout testing error

--- backend/test/server.test.js
+++ backend/test/server.test.js
@@ -72,8 +72,9 @@ describe('Slate API', () => {
         });
 
         it('valid token - should return 200 and token should be invalidated', done => {
-            request(app.app).post('/api/log-out').set('Cookie', ['authToken=u6P3AojGYYrywrRQ']).expect(200).end((err, res) => {
-                request(app.app).post('/api/authenticate').set('Cookie', ['authToken=u6P3AojGYYrywrRQ']).expect(200).end((err, res) => {
+            const agent = request.agent(app.app);
+            agent.post('/api/log-out').set('Cookie', ['authToken=u6P3AojGYYrywrRQ']).expect(200).end((err, res) => {
+                agent.post('/api/authenticate').set('Cookie', ['authToken=u6P3AojGYYrywrRQ']).expect(200).end((err, res) => {
                     assert.deepEqual(res.body, {success: false});
                     done();
                 });
