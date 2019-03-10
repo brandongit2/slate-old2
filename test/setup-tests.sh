8709ec791f71518feee97f6da667a1115969026a
Michael Pu <michael.pu123@gmail.com>
2019-03-09 22:05:50 -0500

    Try fixing nginx

--- test/setup-tests.sh
+++ test/setup-tests.sh
@@ -2,7 +2,6 @@
 sudo apt-get update
 sudo apt-get install nginx
 sudo mv slate.nginx.test.conf /etc/nginx/sites-available/default
-sudo ln -s /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
+sudo /etc/nginx/sites-enabled/default
+sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 sudo nginx
-
-
