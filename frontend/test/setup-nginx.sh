147dce4b5afef0e5daa255baea387ab2115cf75c
Michael Pu <michael.pu123@gmail.com>
2019-03-09 22:59:15 -0500

    Sleep after reloading nginx

--- frontend/test/setup-nginx.sh
+++ frontend/test/setup-nginx.sh
@@ -5,3 +5,4 @@ sudo mv slate.nginx.test.conf /etc/nginx/sites-available/default
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 sudo service nginx reload
+sleep 20
