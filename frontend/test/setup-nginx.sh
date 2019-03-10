536214660f326d669fc2ba02fead3d3c88be159e
Michael Pu <michael.pu123@gmail.com>
2019-03-09 22:38:49 -0500

    Update nginx set up script

--- frontend/test/setup-nginx.sh
+++ frontend/test/setup-nginx.sh
@@ -2,6 +2,6 @@
 sudo apt-get update
 sudo apt-get install nginx
 sudo mv slate.nginx.test.conf /etc/nginx/sites-available/default
-sudo /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 sudo service nginx reload
