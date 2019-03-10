eb9e8089f11842214e11c1552e77a78c0c8ce158
Michael Pu <michael.pu123@gmail.com>
2019-03-09 22:03:46 -0500

    Update nginx

--- test/setup-tests.sh
+++ test/setup-tests.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt-get install nginx
 sudo mv slate.nginx.test.conf /etc/nginx/sites-available/default
-sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+sudo ln -s /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
 sudo nginx
 
 
