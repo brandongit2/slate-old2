66fbee0fc2f96846b4217d3740111980342c3b29
Michael Pu <michael.pu123@gmail.com>
2019-03-09 22:55:58 -0500

    Travis is hard to configure.

--- frontend/test/setup-nginx.sh
+++ frontend/test/setup-nginx.sh
@@ -3,5 +3,5 @@ sudo apt-get update
 sudo apt-get install nginx
 sudo mv slate.nginx.test.conf /etc/nginx/sites-available/default
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
+sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 sudo service nginx reload
