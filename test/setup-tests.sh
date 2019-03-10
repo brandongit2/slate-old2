c85599017645b72d7647d84b179fc538caee4df0
Michael Pu <michael.pu123@gmail.com>
2019-03-09 22:07:42 -0500

    Reload nginx config after adding new config

--- test/setup-tests.sh
+++ test/setup-tests.sh
@@ -4,4 +4,4 @@ sudo apt-get install nginx
 sudo mv slate.nginx.test.conf /etc/nginx/sites-available/default
 sudo /etc/nginx/sites-enabled/default
 sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-sudo nginx
+sudo service nginx reload
