9aa4a7931b601aa5826261689ac9e8cee7282dc7
Michael Pu <michael.pu123@gmail.com>
2019-03-09 23:07:14 -0500

    Finalize frontend tests

--- frontend/test/setup-nginx.sh
+++ frontend/test/setup-nginx.sh
@@ -5,4 +5,3 @@ sudo mv slate.nginx.test.conf /etc/nginx/sites-available/default
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 sudo service nginx reload
-sleep 20
