ca3af6852483e10a34b9d903b0b95e3a70a23f67
Michael Pu <michael.pu123@gmail.com>
2019-04-19 16:32:10 -0400

    Fixed wrong login message

--- backend/test/server/user.test.js
+++ backend/test/server/user.test.js
@@ -14,7 +14,7 @@ describe('auth functions', () => {
         it('empty body - should return 200 and not succeed', done => {
             request(app.app).post('/api/authenticate').expect(200).end((err, res) => {
                 if (err) throw err;
-                assert.deepEqual(res.body, {success: false});
+                assert.deepEqual(res.body, { success: false });
                 done();
             });
         });
@@ -22,7 +22,7 @@ describe('auth functions', () => {
         it('invalid token - should return 200 and not succeed', done => {
             request(app.app).post('/api/authenticate').set('Cookie', ['authToken=thisshouldfail']).expect(200).end((err, res) => {
                 if (err) throw err;
-                assert.deepEqual(res.body, {success: false});
+                assert.deepEqual(res.body, { success: false });
                 done();
             });
         });
@@ -30,7 +30,7 @@ describe('auth functions', () => {
         it('expired token - should return 200 and not succeed', done => {
             request(app.app).post('/api/authenticate').set('Cookie', ['authToken=9cZ8R3yLIzoFnoCD']).expect(200).end((err, res) => {
                 if (err) throw err;
-                assert.deepEqual(res.body, {success: false});
+                assert.deepEqual(res.body, { success: false });
                 done();
             });
         });
@@ -40,15 +40,15 @@ describe('auth functions', () => {
                 if (err) throw err;
                 assert.deepEqual(res.body, {
                     success: true,
-                    user:    {
-                        email:          'brandononline2@gmail.com',
-                        first_name:     'Brandon',
-                        id:             19,
-                        last_name:      'Tsang',
+                    user: {
+                        email: 'brandononline2@gmail.com',
+                        first_name: 'Brandon',
+                        id: 19,
+                        last_name: 'Tsang',
                         password_reset: 0,
-                        permissions:    5,
-                        valid_email:    1,
-                        theme:          'light'
+                        permissions: 5,
+                        valid_email: 1,
+                        theme: 'light'
                     }
                 });
                 done();
@@ -64,38 +64,38 @@ describe('auth functions', () => {
 
     describe('/api/log-in', () => {
         it('invalid email - should return 200 and not succeed', done => {
-            request(app.app).post('/api/log-in').send({email: 'novalid@example.com', password: 't6se;xdroighylsexuirghp;y5ier', stayLoggedIn: true}).expect(200).end((err, res) => {
+            request(app.app).post('/api/log-in').send({ email: 'novalid@example.com', password: 't6se;xdroighylsexuirghp;y5ier', stayLoggedIn: true }).expect(200).end((err, res) => {
                 if (err) throw err;
-                assert.deepEqual(res.body, {success: false});
+                assert.deepEqual(res.body, { success: false, error: 'invalid login' });
                 done();
             });
         });
 
         it('invalid password - should return 200 and not succeed', done => {
-            request(app.app).post('/api/log-in').send({email: 'test@example.com', password: 'wrongpassword', stayLoggedIn: true}).expect(200).end((err, res) => {
+            request(app.app).post('/api/log-in').send({ email: 'test@example.com', password: 'wrongpassword', stayLoggedIn: true }).expect(200).end((err, res) => {
                 if (err) throw err;
-                assert.deepEqual(res.body, {success: false});
+                assert.deepEqual(res.body, { success: false, error: 'invalid login' });
                 done();
             });
         });
 
         it('valid email and password, stay logged in - should return 200 and succeed', done => {
             const agent = request.agent(app.app);
-            agent.post('/api/log-in').send({email: 'test@example.com', password: 'h9fu84whj8j9i', stayLoggedIn: true}).expect(200).end((err, res) => {
+            agent.post('/api/log-in').send({ email: 'test@example.com', password: 'h9fu84whj8j9i', stayLoggedIn: true }).expect(200).end((err, res) => {
                 if (err) throw err;
-                assert.deepEqual(res.body, {success: true});
+                assert.deepEqual(res.body, { success: true });
                 agent.post('/api/authenticate').expect(200).end((err, res) => {
                     assert.deepEqual(res.body, {
                         success: true,
-                        user:    {
-                            id:             21,
-                            first_name:     'Test',
-                            last_name:      'Account',
-                            email:          'test@example.com',
-                            valid_email:    1,
-                            permissions:    5,
+                        user: {
+                            id: 21,
+                            first_name: 'Test',
+                            last_name: 'Account',
+                            email: 'test@example.com',
+                            valid_email: 1,
+                            permissions: 5,
                             password_reset: 0,
-                            theme:          'light'
+                            theme: 'light'
                         }
                     });
                     done();
@@ -105,21 +105,21 @@ describe('auth functions', () => {
 
         it('valid email and password, not stay logged in - should return 200 and succeed', done => {
             const agent = request.agent(app.app);
-            agent.post('/api/log-in').send({email: 'test@example.com', password: 'h9fu84whj8j9i', stayLoggedIn: false}).expect(200).end((err, res) => {
+            agent.post('/api/log-in').send({ email: 'test@example.com', password: 'h9fu84whj8j9i', stayLoggedIn: false }).expect(200).end((err, res) => {
                 if (err) throw err;
-                assert.deepEqual(res.body, {success: true});
+                assert.deepEqual(res.body, { success: true });
                 agent.post('/api/authenticate').expect(200).end((err, res) => {
                     assert.deepEqual(res.body, {
                         success: true,
-                        user:    {
-                            id:             21,
-                            first_name:     'Test',
-                            last_name:      'Account',
-                            email:          'test@example.com',
-                            valid_email:    1,
-                            permissions:    5,
+                        user: {
+                            id: 21,
+                            first_name: 'Test',
+                            last_name: 'Account',
+                            email: 'test@example.com',
+                            valid_email: 1,
+                            permissions: 5,
                             password_reset: 0,
-                            theme:          'light'
+                            theme: 'light'
                         }
                     });
                     done();
@@ -152,7 +152,7 @@ describe('auth functions', () => {
                 assert.equal(res.text, 'Logged out.');
                 agent.post('/api/authenticate').set('Cookie', ['authToken=u6P3AojGYYrywrRQ']).expect(200).end((err, res) => {
                     if (err) throw err;
-                    assert.deepEqual(res.body, {success: false});
+                    assert.deepEqual(res.body, { success: false });
                     done();
                 });
             });
@@ -161,17 +161,17 @@ describe('auth functions', () => {
 
     describe('/api/reset-password', () => {
         it('valid email - should return 200 and succeed', done => {
-            request(app.app).post('/api/reset-password').send({email: 'forgetful2@example.com'}).expect(200).end((err, res) => {
+            request(app.app).post('/api/reset-password').send({ email: 'forgetful2@example.com' }).expect(200).end((err, res) => {
                 if (err) throw err;
-                assert.deepEqual(res.body, {success: true});
+                assert.deepEqual(res.body, { success: true });
                 done();
             });
         });
 
         it('invalid email - should return 200 and succeed', done => {
-            request(app.app).post('/api/reset-password').send({email: 'doesnotexist@example.com'}).expect(200).end((err, res) => {
+            request(app.app).post('/api/reset-password').send({ email: 'doesnotexist@example.com' }).expect(200).end((err, res) => {
                 if (err) throw err;
-                assert.deepEqual(res.body, {success: false, error: 'user not found'});
+                assert.deepEqual(res.body, { success: false, error: 'user not found' });
                 done();
             });
         });
@@ -179,23 +179,23 @@ describe('auth functions', () => {
 
     describe('/api/change-password', () => {
         it('valid query - should return 200 and succeed', done => {
-            request(app.app).post('/api/change-password').send({type: 'query', query: 'VD8d21rhf', password: '1f2vV4sf1s'}).expect(200).end((err, res) => {
+            request(app.app).post('/api/change-password').send({ type: 'query', query: 'VD8d21rhf', password: '1f2vV4sf1s' }).expect(200).end((err, res) => {
                 if (err) throw err;
-                assert.deepEqual(res.body, {success: true});
-                request(app.app).post('/api/log-in').send({email: 'forgetful@example.com', password: '1f2vV4sf1s', stayLoggedIn: true}).expect(200).end((err, res) => {
+                assert.deepEqual(res.body, { success: true });
+                request(app.app).post('/api/log-in').send({ email: 'forgetful@example.com', password: '1f2vV4sf1s', stayLoggedIn: true }).expect(200).end((err, res) => {
                     if (err) throw err;
-                    assert.deepEqual(res.body, {success: true});
+                    assert.deepEqual(res.body, { success: true });
                     done();
                 });
             });
         });
 
         it('invalid query - should return 200 and fail', done => {
-            request(app.app).post('/api/change-password').send({type: 'query', query: 'nonexistantquery'}).expect(200).end((err, res) => {
+            request(app.app).post('/api/change-password').send({ type: 'query', query: 'nonexistantquery' }).expect(200).end((err, res) => {
                 if (err) throw err;
                 assert.deepEqual(res.body, {
                     success: false,
-                    error:   'query not in database'
+                    error: 'query not in database'
                 });
                 done();
             });
@@ -206,7 +206,7 @@ describe('auth functions', () => {
                 if (err) throw err;
                 assert.deepEqual(res.body, {
                     success: false,
-                    error:   'query expected'
+                    error: 'query expected'
                 });
                 done();
             });
@@ -223,31 +223,31 @@ describe('auth functions', () => {
         it('no query - should return 200 and not succeed', done => {
             request(app.app).post('/api/verify').expect(200).end((err, res) => {
                 if (err) throw err;
-                assert.deepEqual(res.body, {success: false, error: 'query expected'});
+                assert.deepEqual(res.body, { success: false, error: 'query expected' });
                 done();
             });
         });
 
         it('invalid query - should return 200 and not succeed', done => {
-            request(app.app).post('/api/verify').send({query: 'nonexistantquery'}).expect(200).end((err, res) => {
+            request(app.app).post('/api/verify').send({ query: 'nonexistantquery' }).expect(200).end((err, res) => {
                 if (err) throw err;
-                assert.deepEqual(res.body, {success: false, error: 'query not in database'});
+                assert.deepEqual(res.body, { success: false, error: 'query not in database' });
                 done();
             });
         });
 
         it('expired query - should return 200 and not succeed', done => {
-            request(app.app).post('/api/verify').send({query: 'VD8d21rhf'}).expect(200).end((err, res) => {
+            request(app.app).post('/api/verify').send({ query: 'VD8d21rhf' }).expect(200).end((err, res) => {
                 if (err) throw err;
-                assert.deepEqual(res.body, {success: false, error: 'query not in database'});
+                assert.deepEqual(res.body, { success: false, error: 'query not in database' });
                 done();
             });
         });
 
         it('valid query - should return 200 and succeed', done => {
-            request(app.app).post('/api/verify').send({query: 'Xn58MHrhf'}).expect(200).end((err, res) => {
+            request(app.app).post('/api/verify').send({ query: 'Xn58MHrhf' }).expect(200).end((err, res) => {
                 if (err) throw err;
-                assert.deepEqual(res.body, {success: true});
+                assert.deepEqual(res.body, { success: true });
                 done();
             });
         });
