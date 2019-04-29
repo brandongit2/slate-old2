503c27e4deef3b177e7ec97af81ae889146e80ca
Brandon Tsang <brandononline2@gmail.com>
2019-04-29 11:00:08 -0400

    Removed Sendgrid as an email provider.

--- backend/src/emails/email.js
+++ backend/src/emails/email.js
@@ -3,24 +3,9 @@
  */
 
 const nodemailer = require('nodemailer');
-const sgMail = require('@sendgrid/mail');
 
-const {email: credentials} = require('../config.json');
-
-if (credentials.provider === 'sendgrid') {
-    sgMail.setApiKey(credentials.apiKey);
-    exports.send = email => {
-        email.from = credentials.from;
-        sgMail.send(email);
-    };
-} else if (credentials.provider === 'smtp') {
-    const transporter = nodemailer.createTransport(credentials.credentials);
-    exports.send = email => {
-        email.from = credentials.from;
-        transporter.sendMail(email);
-    };
-} else if (credentials.provider === 'null') {
-    exports.send = email => {};
-} else {
-    console.error('Invalid email provider: ' + credentials.provider);
-}
+const transporter = nodemailer.createTransport();
+exports.send = email => {
+    email.from = 'Slate <no-reply@slate.cx>';
+    transporter.sendMail(email);
+};
